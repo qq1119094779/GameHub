672423c7aa27c645211b5799027dc22c54bf81d8
unknown <subingcheng@playhudong.com>
2019-07-12 00:03:18 +0800

    首页轮播数据获取渲染

---
+++ js/view/index.js
@@ -0,0 +1,39 @@
+/**
+ * 试玩轮播数据获取
+ * */
+$(function () {
+  let sendJson = {classify: 1}
+  $.ajax({
+    type: "POST",
+    contentType: 'application/json',
+    url: baseUrl + "/gameHub/home/showBanner",
+    dataType: "json",
+    data: JSON.stringify(sendJson),
+    success (data) {
+      console.log(111, data);// 成功之后执行这里面的代码
+      let html = ''
+      if (data) {
+        for (let i = 0, len = data.length; i < len; i++) {
+          html += `<div class="swiper-slide" style=" background-image: url(${data[i].bannerUrl});">
+                      <div class="info txt-l">
+                        <div class="title mb20">
+                          <h2 class="caption">${data[i].gameName}</h2>
+                          <p class="date">${data[i].createTime}</p>
+                          <p class="tag"><span><em>RPG</em></span><span><em>射击</em></span></p>
+                        </div>
+                        <p class="desc">${data[i].briefIntroduction}</p>
+                        <div class="focus">
+                          <span><i class="icon iconfont mr5">&#xe62b;</i><em>${data[i].favoriteCount}</em></span>
+                          <span><i class="icon iconfont mr5">&#xe6b3;</i><em>${data[i].pointRatio}</em></span>
+                        </div>
+                      </div>
+                  </div>`
+        }
+        $('#indexslider .swiper-wrapper').html(html)
+      }
+    },
+    error (e) {
+      console.log(222, e)//请求失败是执行这里的函数
+    }
+   })
+})
