bf80f4b0fe7facf0a9c0d554f35854d0af6362d1
qq1119094779 <1119094779@qq.com>
2019-07-12 12:07:42 +0800

    登陆注册

--- js/common.js
+++ js/common.js
@@ -1,1 +1,35 @@
 const baseUrl = 'http://106.14.206.226:8080'
+const userId = localStorage.id
+$(function () {
+    // 获取当前用户信息
+    if (userId && $('#head .login_status')) {
+        let sendJSon = {userId: userId}
+        $.ajax({
+            type: 'POST',
+            contentType: 'application/json',
+            dataType: 'json',
+            url: `${baseUrl}/gameHub/user/personalData`,
+            data: JSON.stringify(sendJSon),
+            success: function (data) {
+                console.log(data)
+                $('#head .login_status').hide()
+                $('#head .head-right').append(`
+                <div class="user-data fr clearfix">
+                    <div class="user-nickname fl">${data.personData.nickName}</div>
+                    <div class="user-header fl" id="user-header" style="background-image: url(${data.personData.headUrl})"></div>
+                    <div class="fl sign-out" id="sign-out">退出</div>
+                </div>
+                `)
+            },
+            error: function () {
+            }
+        })
+        $(document).on('click', '#sign-out', function(){
+            delete localStorage.id
+            location.reload()
+        })
+        $(document).on('click', '#user-header', function(){
+            window.location.href = '/editor_publish.html'
+        })
+    }
+})
---
+++ js/view/login.js
@@ -0,0 +1,49 @@
+
+$(function () {
+    let login = () => {
+        let sendJSon = {
+            email: $('.email').val(),
+            password: $('.password').val()
+        }
+        if (!sendJSon.email) {
+            tipAlert('请输入邮箱')
+            return
+        } else if (!sendJSon.password) {
+            tipAlert('请输入密码')
+            return
+        }
+        sendJSon = JSON.stringify(sendJSon)
+        $.ajax({
+            type: 'POST',
+            contentType: 'application/json',
+            dataType: 'json',
+            url: `${baseUrl}/gameHub/user/login`,
+            data: sendJSon,
+            success: function (data) {
+                if (!data.code || data.code == 'false') {
+                    tipAlert(data.errorMessage)
+                } else {
+                    tipAlert(data.success)
+                    localStorage.id = data.id
+                    window.location.href = '/index.html'
+                    // setTimeout(function () {
+                    // }, 3000)
+                }
+            },
+            error: function (err) {
+                console.log(err)
+            }
+        })
+    }
+    $('#login').on('click', login)
+    $('#email').on('keydown', (event) => {
+        if(event.keyCode==13){
+            login()
+        }
+    })
+    $('#password').on('keydown', (event) => {
+        if(event.keyCode==13){
+            login()
+        }
+    })
+})
